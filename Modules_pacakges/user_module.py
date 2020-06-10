60132ec1107692faa156fec47ea89dbb2f8c761a
Anjan Kumar <41472950+anjan111@users.noreply.github.com>
2020-06-10 08:03:53 +0530

    Add files via upload

---
+++ Modules_pacakges/user_module.py
@@ -0,0 +1,11 @@
+#user_module.py
+# This is user defined modules
+'''
+which contains user defina=ed function defination
+'''
+def fun1():
+    print "fun1"
+def fun2():
+    print "fun2"
+def fun3():
+    print "fun3"
